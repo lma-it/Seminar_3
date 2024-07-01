98e04833e8ee934933c02603d3c8f6e056748cd2
Михаил Лубенцов <wispergently061@gmail.com>
2024-07-01 16:32:48 +0500

    Создал проект, добавил зависимости. Первый коммит.

---
+++ OOP/Task1.java
@@ -0,0 +1,12 @@
+package OOP;
+import static print_module.print_library.println;
+
+/**
+ * Task1
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        println("Hello");
+    }
+}
