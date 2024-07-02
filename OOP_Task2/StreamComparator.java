753c5918fdf5c16c838e5a583e3fbff3155540c5
Михаил Лубенцов <wispergently061@gmail.com>
2024-07-02 17:51:35 +0500

    Добавлен JavaDoc для класcа StreamComparator.

--- OOP_Task2/StreamComparator.java
+++ OOP_Task2/StreamComparator.java
@@ -1,6 +1,14 @@
 package OOP_Task2;
 import java.util.Comparator;
 
+/**
+ * Класс, который позволяет реализовать сравнение потоков по их размеру, а именно количеству групп в каждом потоке.
+ * 
+ * <p>
+ * Имплементируется от {@link Comparator} и переопределяет метод {@code public int compare(StudentStream o1, StudentStream o2)},
+ * который уже позволяет провести сравнение потоков для их последующей сортировки.
+ * </p>
+ */
 public class StreamComparator implements Comparator<StudentStream>{
 
     @Override
